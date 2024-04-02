271e70bb6fc26d46065cc465a773df84f7dce76b
Nicolas Wormser <nicolas@codewithflair.org>
2024-04-02 15:58:27 +0200

    Finish JS step 2 part 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -3,3 +3,11 @@ const avatar = document.querySelector("img[alt=avatar]")
 avatar.addEventListener("click", function () {
   avatar.src = "image/avatar.svg"
 })
+
+const button = document.querySelector("button")
+const firstName = document.querySelector("#firstname")
+
+button.addEventListener("click", function () {
+  firstName.textContent = prompt("Enter your name:")
+  firstName.style.color = "white"
+})
