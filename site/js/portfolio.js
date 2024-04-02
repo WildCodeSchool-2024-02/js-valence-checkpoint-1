c76e829a8fee2b4916045862b44b65462b5d83dc
Nicolas Wormser <nicolas@codewithflair.org>
2024-04-02 16:05:24 +0200

    Finish JS step 3 part 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -6,8 +6,10 @@ avatar.addEventListener("click", function () {
 
 const button = document.querySelector("button")
 const firstName = document.querySelector("#firstname")
-
+const pinkBg = document.querySelector(".pink-bg")
 button.addEventListener("click", function () {
+  const color = prompt("Enter color:")
+  pinkBg.style.backgroundColor = color
   firstName.textContent = prompt("Enter your name:")
   firstName.style.color = "white"
 })
