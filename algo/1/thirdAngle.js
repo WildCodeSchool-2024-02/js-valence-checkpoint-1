befbf0685d41b12f3661a3030ab68457ece8348b
lucas07300 <161328245+lucas07300@users.noreply.github.com>
2024-04-02 10:09:26 +0200

    algo1 terminer

--- algo/1/thirdAngle.js
+++ algo/1/thirdAngle.js
@@ -7,7 +7,13 @@ thirdAngle(20, 80) doit retourner 80
 */
 
 function thirdAngle(a, b) {
-  // Your code here !
+  const sommeDesAngles = 180;
+  const thirdAngle = sommeDesAngles - a - b;
+  return thirdAngle
+  const a = 90
+  const b = 30
+  const third = thirdAngle (a, b)
+  console.log ("le troisième angle est :"third)
 }
 
 module.exports = thirdAngle;
