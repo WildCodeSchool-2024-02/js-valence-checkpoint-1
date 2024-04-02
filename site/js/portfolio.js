05ea8ad51ee4f94987d2a4af35ed2cec0f802ca2
IChris2611I <pachris2611@gmail.com>
2024-04-02 12:20:58 +0200

    fix super bonus CSS

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -3,6 +3,8 @@ const firstName = document.getElementById("firstname")
 const addNewName = document.getElementById("addNewName")
 const bgColor = document.getElementsByClassName("pink-bg")
 const textColor= document.querySelectorAll("h4")
+const links= document.querySelectorAll("a")
+const listTools = document.querySelectorAll("ul.front-dev-tools > li")
 avatarImage.addEventListener('click', function () {
         avatarImage.src = "./image/avatar-bis.png";
 })
@@ -13,11 +15,20 @@ addNewName.addEventListener('click', function(){
 
     // NOT WORKING =  Uncaught TypeError: Cannot read properties of null (reading 'style')
     // FIXED = FORGOT ON " ON THE HTML ID ..... NO COMMENT
+    let color = prompt('What color do you want  ? ')
+    for( let i = 0; i < bgColor.length; i++ ) {
+        bgColor[i].style.backgroundColor = color;
+    }
+    for( let i = 0; i < textColor.length; i++ ) {
+        textColor[i].style.color = color;
 
-    for( let i = 0; i< bgColor.length && textColor.length; i++ ) {
-        let allColor = prompt('What color do you want  ? ')
-        bgColor[i].style.backgroundColor = allColor
-        textColor[i].style.color = allColor
+}
+})
+
+addNewName.addEventListener('click', function(){
+    let colorLink = prompt('What color do you want for the LINK  ? ')
+    for( let i = 0; i< links.length; i++ ) {
+        links[i].style.color = colorLink
     }
 })
 
