c6d4331bd34910e75c7e46ec1d36932f26373060
derimer <jderimer@gmail.com>
2024-04-05 09:09:50 +0200

    modification background js

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -2,13 +2,15 @@ const btn = document.createElement("button");
 const firstname = document.querySelector("#firstname");
 const modifie = document.querySelector("modifie");
 const mouvimage = document.querySelector("#ima");
-
+const changBackground = document.querySelector(".pink-bg ");
 mouvimage.addEventListener("click", function () {
   mouvimage.src = "/site/image/avatar-bis.png";
 });
 
 modifie = firstname.addEventListener("click", () => {
   const nom = prompt("entre ton prénom");
+  changBackground.style.backgroundColor = "black";
   firstname.innerText = nom;
+
   firstname.style.color = "#fff";
 });
